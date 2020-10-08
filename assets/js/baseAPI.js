884a273095e32b97fd00dc7350dc2be83dfa4e65
gggzj <1346072714@qq.com>
2020-10-08 20:57:31 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,19 @@
 var baseUrl = 'http://ajax.frontend.itheima.net';
 $.ajaxPrefilter(function(options) {
     options.url = baseUrl + options.url
-    console.log(options.url);
+        // console.log(options.url);
+
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
 })
